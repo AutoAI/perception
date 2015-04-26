21f9a14f2eb1acb99690e5656fca2c4ff5a56315
Parth Mehrotra <parth.mehrotra.cs@gmail.com>
2015-04-25 23:20:45 -0400

    Added get method implementation

--- Assignments/NdArray.cpp
+++ Assignments/NdArray.cpp
@@ -27,5 +27,12 @@ void NdArray<T>::set(char* dimensions, T value) {
     array[index] = value;
 }
 
-
+template<typename T>
+T NdArray<T>::get(char* dimensions) {
+    char index = 0;
+    for (char i = 0; i < numDimensions; i++) {
+        index += dimensions[i] * sizes[i];
+    }
+    return array[index];
+}
 
