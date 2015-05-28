aa9749d1f2c7a04746af03ad409f3a2734920723
tvanderstad <t.vanderstad@gmail.com>
2015-05-28 15:12:06 -0400

    reorganized src a litte, preparation for working with plotting

--- src/Plotting/TestingPlotter.py
+++ src/Plotting/TestingPlotter.py
@@ -1,14 +1,14 @@
 from numpy import *
-import pylab as p
-#import matplotlib.axes3d as p3
+from matplotlib import pyplot as p
+import matplotlib
 import mpl_toolkits.mplot3d.axes3d as p3
 from fractions import *
-
+from matplotlib import *
 
 from scipy.interpolate import *
 
 #plotting
-plotting = '''
+#plotting = '''
 #Set up x, y, z
 x = [2,1,3]
 y = [2,1,3]
@@ -16,11 +16,12 @@ z = [2,1,3]
 
 fig=p.figure()
 ax = p3.Axes3D(fig)
-ax.plot_wireframe(x,y,z)
+ax.scatter(x,y,z)
 ax.set_xlabel('X')
 ax.set_ylabel('Y')
 ax.set_zlabel('Z')
-p.show()'''
+p.show()
+#'''
 
 #Image normailztion
 
