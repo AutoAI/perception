d6fca63bf1aab41bf1754efc4b00ab827575c43e
frogeyedpeas <sid.ghoshal@yahoo.com>
2015-05-28 03:58:17 -0400

    Updates

--- src/TestingPlotterPart2.py
+++ src/TestingPlotterPart2.py
@@ -1,4 +1,11 @@
 from TestingPlotter import *
+from numpy import *
+import pylab as p
+import mpl_toolkits.mplot3d.axes3d as p3
+from fractions import *
+
+
+from scipy.interpolate import *
 
 x = raw_input("Enter Points: ")
 
@@ -15,7 +22,23 @@ w = tuplesort(x)
 
 for l in w:
     #Needs to be repaired
+    q1 = interpoints(l[0],l[1])
+    q2 = interpoints(l[0],l[2])
+    q3 = interpoints(l[1],l[2])
+    x1 = linspace(l[0][0], l[1][0],20)
+    x2= linspace(l[0][0], l[2][0],20)
+    x3 = linspace(l[1][0], l[2][0],20)
 
-print w
+    
+    f1 = interp1d([m[0] for m in q1],[m[1] for m in q1])
+    f2 = interp1d([m[0] for m in q2], [m[1] for m in q2])
+    f3 = interp1d([m[0] for m in q3], [m[1] for m in q3])
+
+    p.plot(co(x1),[f1(i) for i in co(x1)])
+    p.plot(co(x2),[f2(i) for i in co(x2)])
+    p.plot(co(x3),[f3(i) for i in co(x3)])
+
+p.show()
+
 
 
