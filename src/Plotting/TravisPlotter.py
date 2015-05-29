3402efc0fa65d8d331d7e804805fde4142fb56bd
tvanderstad <t.vanderstad@gmail.com>
2015-05-28 21:18:34 -0400

    got the begginings of some interpolation going

--- src/Plotting/TravisPlotter.py
+++ src/Plotting/TravisPlotter.py
@@ -1,4 +1,3 @@
-#parth is  a boss
 from numpy import *
 from matplotlib import pyplot as p
 import matplotlib
@@ -9,9 +8,41 @@ from scipy.interpolate import *
 
 import math
 import random
+
+class InterpolatedImage:
+	def __init__(self, s, n, r):
+		# dimension of image
+		self.s = s
+		# number of points that are going to be processed
+		self.n = n
+		# inradius of the square around which points will be drawn
+		self.r = r
+		# values that will be stored from points
+		self.img = numpy.zeros((s, s, n))
+		# number of values that have been stored at each index
+		self.lengths = numpy.zeros((s, s), numpy.int32)
+
+	def processPoint(self, x, y, z):
+		for i in range(int(max(x-self.r,0)), int(min(x+self.r, self.s - 1))):
+			for j in range(int(max(y-self.r, 0)), int(min(y+self.r, self.s - 1))):
+				self.img[i][j][self.lengths[i][j]] = z
+				self.lengths[i][j] += 1
+
+	def plotAll(self, ax):
+		x = []
+		y = []
+		z = []
+		for i in range(0, self.s):
+			for j in range(0, self.s):
+				for k in range(0, self.lengths[i][j]):
+					x.append(i)
+					y.append(j)
+					z.append(self.img[i][j][k])
+		ax.scatter(x, y, z)
+
 def f(x, y):
 	d = math.sqrt(x**2 + y**2)
-	return math.cos(d)*math.exp(-d)
+	return math.cos(d)*math.exp(-d/4)
 
 def dist2(t1, t2):
 	return (t1[0]-t2[0])**2 + (t1[1]-t2[1])**2
@@ -19,9 +50,15 @@ def dist2(t1, t2):
 def dist(t1, t2):
 	return math.sqrt(dist2(t1, t2))
 
-# Setup the points
-n = 100
-m = 10
+def imgToCoord(x, m, d):
+	return x*2*m/d-m
+
+def coordToImg(x, m, d):
+	return (x+m)*d/(2*m)
+
+# Setting up the input
+n = 80
+m = 6.28
 # n random x values between -m and m
 x = [random.random()*2*m-m for _ in range(0, n)]
 # n random y values between -m and m
@@ -31,4 +68,19 @@ z = []
 for i in range(0, n):
 	z.append(f(x[i], y[i]))
 
+# Setting up the image
+# image is d-by-d pixels
+d = 30
+img1 = InterpolatedImage(d, n, 2)
+for i in range(0, n):
+	img1.processPoint(coordToImg(x[i], m, d), coordToImg(y[i], m, d), coordToImg(z[i], m, d))
 
+# Making the plot
+fig = p.figure()
+ax = p3.Axes3D(fig)
+# ax.scatter(x,y,z) # use this to plot input
+img1.plotAll(ax)
+ax.set_xlabel('X')
+ax.set_ylabel('Y')
+ax.set_zlabel('Z')
+p.show()
