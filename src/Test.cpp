cfe6517490d178f6ea5082b2ae06b080baf7e420
Parth Mehrotra <parth.mehrotra.cs@gmail.com>
2015-05-30 02:42:43 -0400

    simplified Test

--- src/Test.cpp
+++ src/Test.cpp
@@ -6,22 +6,13 @@
 int main(int argc, char **argv){ 
     ros::init(argc, argv, "talker");
     ros::NodeHandle n;
-    ros::Publisher chatter_pub = n.advertise<std_msgs::String>("chatter", 1000);
-    ros::Rate loop_rate(10);
 
-    int count = 0;
-    while (ros::ok()) {
-        std_msgs::String msg;
-        std::stringstream ss;
-        ss<<"Hello World!"<< count;
-        msg.data = ss.str();
+    ros::spinOnce();
 
-        ROS_INFO("%s", msg.data.c_str());
+    if (ros::ok()) {
 
-        chatter_pub.publish(msg);
+        ROS_INFO("hello");
 
-        ros::spinOnce();
-        loop_rate.sleep();
-        ++count;
+
     }
 }
