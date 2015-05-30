dce0bf53f75efb4ec3530a3acc578a472ba0b2b9
Parth Mehrotra <parth.mehrotra.cs@gmail.com>
2015-05-30 02:50:25 -0400

    removed tools from Perception, and started phasing the tests in

--- src/Test.cpp
+++ src/Test.cpp
@@ -1,7 +1,86 @@
 #include "ros/ros.h"
 #include "std_msgs/String.h"
 
+#include "NdArray.cpp"
+#include <string>
+
 #include <sstream>
+
+using namespace std;
+void testNdArray() {
+    char bounds[3] = {3, 3, 3};
+    NdArray<string> array(3, bounds);
+
+    char location[3] = {0, 0, 0};
+    string value = "asdf";
+    array.set(location, value);
+    
+    if (value.compare(array.get(location)) == 0) {
+        ROS_INFO("PASS");
+    } else {
+        ROS_INFO("FAIL");
+    }
+
+    location[0] = 0;
+    location[1] = 2;
+    location[2] = 0;
+    value = "sdflj";
+    array.set(location, value);
+    
+    if (value.compare(array.get(location)) == 0) {
+        ROS_INFO("PASS");
+    } else {
+        ROS_INFO("FAIL");
+    }
+    
+    location[0] = 2;
+    location[1] = 0;
+    location[2] = 2;
+    value = "sdflkjsd";
+    array.set(location, value);
+    
+    if (value.compare(array.get(location)) == 0) {
+        ROS_INFO("PASS");
+    } else {
+        ROS_INFO("FAIL");
+    }
+
+    location[0] = 2;
+    location[1] = 1;
+    location[2] = 1;
+    value = "lkjsd";
+    array.set(location, value);
+    
+    if (value.compare(array.get(location)) == 0) {
+        ROS_INFO("PASS");
+    } else {
+        ROS_INFO("FAIL");
+    }
+    
+    location[0] = 1;
+    location[1] = 1;
+    location[1] = 1;
+    value = "sdflkjsd";
+    array.set(location, value);
+    
+    if (value.compare(array.get(location)) == 0) {
+        ROS_INFO("PASS");
+    } else {
+        ROS_INFO("FAIL");
+    }
+
+    location[0] = 1;
+    location[1] = 2;
+    location[2] = 1;
+    value = "dflkjsd";
+    array.set(location, value);
+    
+    if (value.compare(array.get(location)) == 0) {
+        ROS_INFO("PASS");
+    } else {
+        ROS_INFO("FAIL");
+    }
+}
 
 int main(int argc, char **argv){ 
     ros::init(argc, argv, "talker");
@@ -10,9 +89,6 @@ int main(int argc, char **argv){
     ros::spinOnce();
 
     if (ros::ok()) {
-
-        ROS_INFO("hello");
-
-
+        testNdArray();
     }
 }
