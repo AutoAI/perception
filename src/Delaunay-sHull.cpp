8d0351f2c8b589e8de39178d4e1465061f86cf41
tvanderstad <t.vanderstad@gmail.com>
2015-06-02 16:21:44 -0400

    got set up for triangulation + tesselation in c++

--- src/Delaunay-sHull.cpp
+++ src/Delaunay-sHull.cpp
@@ -3,26 +3,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <algorithm>
+#include <functional>
+#include <iterator>
 
 #include "NdArray.cpp"
+
+#define gridRes 100
+#define numPoints 1000
+#define pointRange 1000
 
 using namespace std;
 
 int main(int argc, char **argv){ 
-    ros::init(argc, argv, "Run");
-
+	ros::init(argc, argv, "Run");
 	ros::NodeHandle n;
-
 	ros::spinOnce();
-
 	if (ros::ok()) {
-            ROS_INFO("Parth Is good");
-			// initialize random seed
-			srand(time(NULL));
-			// make a grid
-			char d = 100;
-			char s[2] = {d, d};
-			NdArray<char> grid(2, s);
-			return 0;
+		// initialize random seed
+		srand(time(NULL));
+
+		// make some random points
+		size_t s2[2] = {numPoints, 3};
+		NdArray<float> points(2, s2);
+		for(size_t i = 0; i+1 < numPoints*3; i+=3){
+			points.set(i, static_cast<float>(rand())/(static_cast<float>(RAND_MAX/(2*pointRange)))-pointRange);
+			points.set(i+1, static_cast<float>(rand())/(static_cast<float>(RAND_MAX/(2*pointRange)))-pointRange);
+		}
+
+		return 0;
 	}
 }
