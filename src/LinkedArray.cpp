81881ddd3e794bb960e2673228ff03f0011a9341
tvanderstad <t.vanderstad@gmail.com>
2015-06-09 11:18:59 -0400

    still working on these files

--- src/LinkedArray.cpp
+++ src/LinkedArray.cpp
@@ -5,53 +5,70 @@
 
 #pragma once
 
+#include <algorithm>
+
 #include "LinkedArray.h"
 
 LinkedArray::LinkedArray(size_t length) {
     array = new Triple[length];
-    pointers = new size_t[length];
-    head = 0;
-    this->length = length;
-    for(size_t i = 0; i < length; i++)
-        pointers[i] = i + 1;
 }
 
 void LinkedArray::set(size_t index, Triple value) {
-    size_t resultIndex = head;
-    for(size_t i = 0; i < index; i++)
-        resultIndex = pointers[resultIndex];
-    array[resultIndex] = value;
-}
-
-void LinkedArray::setAbsolute(size_t index, Triple value) {
     array[index] = value;
 }
 
-void LinkedArray::reset() {
-    state = head;
-}
-
 Triple LinkedArray::get(size_t index) {
-    size_t resultIndex = head;
-    for(size_t i = 0; i < index; i++)
-        resultIndex = pointers[resultIndex];
-    return array[resultIndex];
-}
-
-Triple LinkedArray::getAbsolute(size_t index) {
     return array[index];
 }
 
-Triple LinkedArray::next() {
-    Triple result = array[state];
-    state = pointers[state];
-    return result;
+void LinkedArray::sort() {
+    size_t mid, right, n1, n2, prev, head;
+    Triple* temp = new Triple[length];
+    size_t* pointers = new size_t[length];
+    for(size_t size = 1; size < length; size *= 2){
+        for(size_t left = 0; left < length-1; left += 2*size){
+            mid = left + size - 1;
+            right = std::min(left + 2*size - 1, length-1);
+            n1 = left;
+            n2 = mid + 1;
+            // merge
+            if(dist2(array[n1], origin) < dist2(array[n2], origin)){
+                head = n1;
+                prev = n1++;
+            }else{
+                head = n2;
+                prev = n2++;
+            }
+            while(n1 <= mid && n2 <= right){
+                if(dist2(array[n1], origin) < dist2(array[n2], origin)){
+                    pointers[prev] = n1;
+                    prev = n1++;
+                }else{
+                    pointers[prev] = n2;
+                    prev = n2++;
+                }
+            }
+            while(n1 <= mid){
+                pointers[prev] = n1;
+                prev = n1++;
+            }
+            while(n2 <= right){
+                pointers[prev] = n2;
+                prev = n2++;
+            }
+            size_t c = head;
+            for(size_t i = left; i <= right; i++){
+                temp[i] = array[c];
+                c = pointers[c];
+            }
+        }
+    }
 }
 
-void LinkedArray::sort(Triple t) {
-
+float LinkedArray::dist2(Triple a, Triple b){
+    return (a.x-b.x)*(a.x-b.x)+(a.y-b.y)*(a.y-b.y);
 }
 
 void LinkedArray::resort(Triple t) {
-
+    
 }
--- src/LinkedArray.h
+++ src/LinkedArray.h
@@ -18,8 +18,8 @@ class LinkedArray {
         // the capacity of the array (not the number of actual elements stored)
         size_t length;
 
-        // the index of the current element for iterating. use reset to make it the first element again
-        size_t state;
+        // origin for comparisons (sorts will be based on distance to this point)
+        Triple origin;
 
         // all you need to make one is the length. the length cannont be changed after making it
         LinkedArray(size_t length);
@@ -27,43 +27,25 @@ class LinkedArray {
         // set a particular element by its list index
         void set(size_t index, Triple value);
 
-        // set a particular element by its array index
-        void setAbsolute(size_t index, Triple value);
-
-        // reset the iteration to the beginning. you should probably reset after making any changes
-        void reset();
-
         // returns a particular element by its list index
         Triple get(size_t index);
 
-        // returns a particular element by its array index
-        Triple getAbsolute(size_t index);
-
-        // to iterate, call this. returns the curent element and increments state
-        Triple next();
-
         // performs an nlogn iterative merge sort. good for when the array is a mess
         // metric is vector distance to parameter to 2D
-        void sort(Triple t);
+        void sort();
 
         // performs an n^2 insertion sort. good for when the list is mostly in order
         // metric is vector distance to parameter to 2D
         void resort(Triple t);
 
-        // straightens out the array so that the ith element of the list is array[i] (for 0(1) access in order)
-        void straighten();
-
     private:
         // the data stored in the structure
         Triple* array;
-        
-        // an array of indices. the element after array[i] is array[pointers[i]]
-        size_t* pointers;
-        
-        // the index of the first element
-        size_t head;
 
-        // a value for temporary use. assumed to be garbage at any time
-        size_t swap;
+        // helper function to merge [l..m] and [m+1..r]
+        void merge(size_t l, size_t m, size_t r);
+
+        // helper function to compute squared distance between two points in 2D
+        float dist2(Triple a, Triple b);
 };
 
