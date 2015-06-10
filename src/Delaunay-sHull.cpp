12a891d8513b167d966c4f7a3bfeae51a46d8e75
Parth Mehrotra <parth.mehrotra.cs@gmail.com>
2015-06-09 22:58:03 -0400

    fixed task list

--- src/Delaunay-sHull.cpp
+++ src/Delaunay-sHull.cpp
@@ -24,21 +24,12 @@ int main(int argc, char **argv){
 		srand(time(NULL));
 
 		// make some random points
-		size_t s2[2] = {numPoints, 3};
-		NdArray<float> points(2, s2);
-		for(size_t i = 0; i+1 < numPoints*3; i+=3){
-			points.set(i, static_cast<float>(rand())/(static_cast<float>(RAND_MAX/(2*pointRange)))-pointRange);
-			points.set(i+1, static_cast<float>(rand())/(static_cast<float>(RAND_MAX/(2*pointRange)))-pointRange);
-		}
 
 		// choose seed point, sort others accorinding to distance from seed
-		
+
+		//create a ds where there are points xyz and a list of of triangls. Triangles will have a Point a, b, c
 
 		// construct initial convex hull (right-hand), determine circum-circle center
-
-
-		// re-sort points according to distance from circum-circle center
-
 
 		// sequentially insert points, adding edges from new point to 'visible' points on the convex hull
 
