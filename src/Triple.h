03eb5852f47a136ce84a01d5bed3c29a7d3fce2a
Parth Mehrotra <parth.mehrotra.cs@gmail.com>
2015-06-17 19:14:19 -0400

    linted Triple.h

--- src/Triple.h
+++ src/Triple.h
@@ -1,12 +1,17 @@
-#pragma once 
+#ifndef SRC_TRIPLE_H_
+#define SRC_TRIPLE_H_
+
+#pragma once
 
 class Triple {
-	public:
-		Triple();
-		Triple(float x, float y, float z);
-		float x;
-		float y; 
-		float z;
-		bool operator==(const Triple &t1);
-		bool operator!=(const Triple &t1);	
+  public:
+    Triple();
+    Triple(float x, float y, float z);
+    float x;
+    float y;
+    float z;
+    bool operator==(const Triple &t1);
+    bool operator!=(const Triple &t1);
 };
+
+#endif // SRC_TRIPLE_H_
