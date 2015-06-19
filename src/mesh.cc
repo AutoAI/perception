e1ef838165cd42b72541f50ac1b8b354453993c9
steverusso <steverusso602@gmail.com>
2015-06-19 19:17:21 -0400

    Broke line 260 into several lines for increased readability.

--- src/mesh.cc
+++ src/mesh.cc
@@ -457,7 +457,12 @@ bool Mesh::isVisible(Triple& a, Triple& d) {
 	}
 
 	for(uint64_t i = 1; i < hull.size(); i++) {
-		if(!((a == *(hull[i-1] -> triple)) || ((d == *(hull[i-1] -> triple))) || (a == *(hull[i] -> triple)) || ((d == *(hull[i] -> triple)))) && testIntersect(*(hull[i-1] -> triple), *(hull[i] -> triple), a, d)) {
+		if(!((a == *(hull[i-1] -> triple)) ||
+				((d == *(hull[i-1] -> triple))) ||
+				(a == *(hull[i] -> triple)) ||
+				((d == *(hull[i] -> triple)))) &&
+				testIntersect(*(hull[i-1] -> triple),
+					*(hull[i] -> triple), a, d)) {
 			return false;
 		}
 	}
