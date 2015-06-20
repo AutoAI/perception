ac419050f559d2fdf0d611f7120368cdd45971d4
Parth Mehrotra <parth.mehrotra.cs@gmail.com>
2015-06-19 21:18:40 -0400

    do correspondance

--- src/run.cc
+++ src/run.cc
@@ -1,5 +1,7 @@
 #include "ros/ros.h"
 
+#include "seeded_depth_map.h"
+
 int main(int argc, char **argv) {
 	ros::init(argc, argv, "Perception_Run");
 	ros::NodeHandle n;
@@ -7,6 +9,8 @@ int main(int argc, char **argv) {
 	ros::spinOnce();
 
 	if (ros::ok()) {
+		SeededDepthMap seed();
+		seed.doCorrespondence();
 		ROS_INFO("Running");
 	}
 }
